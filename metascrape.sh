c94db88b22009fc37480163d67cc1969b4db7bf8
Eric Schulman <erichschulman@erichschulman-OptiPlex-3010>
2020-01-10 17:03:38 -0600

    deleted metascrape sh script

--- metascrape.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "-------starting scrape --------"
-cd /home/erichschulman/Documents/9oclock/
-/home/erichschulman/anaconda3/bin/python metascrape.py
-echo "----------------- backing up files --------------------"
-/usr/bin/rclone sync ~/Documents/9oclock/db/scrape.db remote:9oclock/db/
-echo "--------- end of scrape---------"
